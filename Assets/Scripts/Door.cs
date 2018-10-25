cde253304c3e2dc8f86cff5331f42ec6f8d11844
Neil Hansen <neilmickneilh12@hotmail.com>
2018-10-25 13:46:36 -0400

    added project

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+
+    public float height;
+    // Use this for initialization
+    void Start () {
+        height = this.gameObject.transform.localScale.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveUp()
+    {
+
+        this.gameObject.transform.position += new Vector3(0, height,0);
+    }
+
+    public void MoveDown()
+    {
+        this.gameObject.transform.position -= new Vector3(0, height,0);
+    }
+}
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+
+    public Door[] Gates;
+    public bool DoorOpen = false;
+
+    // Use this for initialization
+    void Start () {
+        Gates = GameObject.FindObjectsOfType<Door>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDoors()
+    {
+       // Debug.Log("OpeningDoors1");
+        for (int i = 0; i < Gates.Length; i++)
+        {
+            Gates[i].MoveUp();
+           // Debug.Log("OpeningDoors");
+        }
+    }
+    public void CloseDoors()
+    {
+        Debug.Log("ClosingDoors1");
+        for (int i = 0; i < Gates.Length; i++)
+        {
+            Gates[i].MoveDown();
+            Debug.Log("ClosingDoors");
+        }
+    }
+
+    public void UpdateDoors()
+    {
+        Debug.Log("Here");
+        Debug.Log(DoorOpen);
+        if (DoorOpen)
+        {
+           
+            Debug.Log("Open");
+            Debug.Log(DoorOpen);
+            OpenDoors();
+        }
+        else
+        {
+          
+            Debug.Log("Closed");
+            CloseDoors();
+        }
+        // DoorSwitch = false;
+        Debug.Log(DoorOpen + "fuck");
+    }
+
+}
