ba6b490dba80d51714e9633a811ef5908822bb06
Neil Hansen <neilmickneilh12@hotmail.com>
2018-10-26 15:15:02 -0400

    added dans and mo's scenes

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -6,6 +6,7 @@ public class DoorController : MonoBehaviour {
 
 
     public Door[] Gates;
+    public SecurtySystem alarm;
     public bool DoorOpen = false;
 
     // Use this for initialization
@@ -15,25 +16,43 @@ public class DoorController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        //testing purposes
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("should be opening");
+            OpenDoors();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("should be closing");
+            CloseDoors();
+        }
 		
 	}
 
     public void OpenDoors()
     {
-       // Debug.Log("OpeningDoors1");
+        alarm.alarmOn();
+        // Debug.Log("OpeningDoors1");
         for (int i = 0; i < Gates.Length; i++)
         {
             Gates[i].MoveUp();
-           // Debug.Log("OpeningDoors");
+            // Debug.Log("OpeningDoors");
+            
         }
     }
     public void CloseDoors()
     {
+        alarm.alarmOff();
         Debug.Log("ClosingDoors1");
         for (int i = 0; i < Gates.Length; i++)
         {
             Gates[i].MoveDown();
             Debug.Log("ClosingDoors");
+            
         }
     }
 
---
+++ Assets/Scripts/SecurtySystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecurtySystem : MonoBehaviour {
+
+
+    [SerializeField] Slider alarmLevel;
+
+    private float alarmProgress;
+    private bool alarmIncrease;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        alarmLevel.value = alarmProgress;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            alarmProgress += 0.25f;
+        }
+
+        if (alarmIncrease == true)
+        {
+            alarmProgress += Time.deltaTime / 30;
+        }
+
+        if (alarmLevel.value > .5)
+        {
+            startTimer();
+            alarmProgress += Time.deltaTime / 40;
+        }
+    }
+
+    /*public void valueChange()
+    {
+        Debug.Log("changing");
+
+    }*/
+
+    private void startTimer()
+    {
+        Debug.Log("Incoming You Suck");
+    }
+
+    public void alarmOn()
+    {
+        Debug.Log("should be moving");
+        alarmIncrease = true;
+    }
+
+    public void alarmOff()
+    {
+        Debug.Log("should be moving");
+        alarmIncrease = false;
+    }
+
+
+}
