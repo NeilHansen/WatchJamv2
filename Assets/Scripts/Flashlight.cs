bfc2df58748255e2460555911cd3925ce471c6c1
Neil Hansen <neilmickneilh12@hotmail.com>
2018-10-26 22:46:26 -0400

    fixed shit

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+
+    public GameObject[] playerCam;
+    private int oldMask;
+	// Use this for initialization
+	void Start () {
+        playerCam = GameObject.FindGameObjectsWithTag("SecurityCam");
+        oldMask = playerCam[0].GetComponent<Camera>().cullingMask;
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Monster")
+        {
+            for (int i = 0; i < playerCam.Length; i++)
+            {
+                playerCam[i].GetComponent<Camera>().cullingMask = -1;
+
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Monster")
+        {
+            for (int i = 0; i < playerCam.Length; i++)
+            {
+                playerCam[i].GetComponent<Camera>().cullingMask = oldMask;
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
