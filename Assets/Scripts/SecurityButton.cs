f4fd45d528d6276c3e5a463e12ca462cf4f5afcf
Dan <browning.dan@gmail.com>
2018-11-09 21:50:25 -0500

    new security stuff

---
+++ Assets/Scripts/SecurityButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityButton : MonoBehaviour {
+
+    public bool isPressed;
+    //public Color buttonColor;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        isPressed = true;
+        //gameObject.GetComponent<Renderer>().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+
+
+
+
+	}
+
+    public void pressed()
+    {
+        isPressed = true;
+        
+    }
+
+    public void unPressed()
+    {
+        isPressed = false;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isPressed)
+            unPressed();
+        else if (!isPressed)
+            pressed();
+    }
+}
