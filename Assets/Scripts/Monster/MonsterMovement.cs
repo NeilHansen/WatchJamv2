54984a87c114f76445838e02c4675e40642c5063
donnytrona <donny_trona@hotmail.com>
2018-11-16 03:48:40 -0500

    Wall Climbing -Removed old nav mesh way -Redid script to use raycast instead

--- Assets/Scripts/Monster/MonsterMovement.cs
+++ Assets/Scripts/Monster/MonsterMovement.cs
@@ -1,115 +1,133 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
 
-public class MonsterMovement : MonoBehaviour {
+public class MonsterMovement : MonoBehaviour
+{
+    public float moveSpeed = 6; // Move speed
+    public float turnSpeed = 90; // Turning speed (degrees/second)
+    public float lerpSpeed = 10; // Smoothing speed
+    public float gravity = 10; // Gravity acceleration(Rounding because decimals suck!)
+    public float flipSpeed = 2.0f; // How fast to flip onto wall
 
-    private bool doOnce = false;
-    private NavMeshAgent agent;
-    Vector3 velocity = Vector2.zero;
-    private Quaternion r;
+    private float distGround; // Distance from character position to ground
+    public float deltaGround = 0.2f; // Character is grounded up to this distance
+    public float flipRange = 1; // Range to detect target wall
 
-    public Camera cam;
+    private bool isGrounded;
+    private bool flipping = false; // Flag "I'm flipping to wall";
 
-    // Use this for initialization
-    void Awake()
+    private Vector3 surfaceNormal; // Current surface normal
+    private Vector3 myNormal; // Character normal
+
+    private Rigidbody rigidbody;
+    private Transform myTransform;
+    private BoxCollider boxCollider; // drag BoxCollider ref in editor
+
+    void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        rigidbody = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        myTransform = transform;
+
+        // Normal starts as character up direction
+        myNormal = transform.up;
+        // Disable physics rotation
+        rigidbody.freezeRotation = true;
+        // Distance from transform.position to ground
+        distGround = boxCollider.size.y - boxCollider.center.y;
     }
 
-    // Use this for initialization
-    void Start () {
-        
+    void FixedUpdate()
+    {
+        // Apply constant weight force according to character normal:
+        rigidbody.AddForce(-gravity * rigidbody.mass * myNormal);
     }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    float x = Input.GetAxis("Horizontal");
-    //    float z = Input.GetAxis("Vertical");
-
-    //    int layerMask = 1 << 15;
-
-    //    RaycastHit hit;
-
-    //    //if (Mathf.Abs(x) > Mathf.Epsilon || Mathf.Abs(z) > Mathf.Epsilon)
-    //    {
-            
-    //        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-45, transform.right) * transform.forward, out hit, 2, layerMask) && !doOnce)
-    //        {
-    //            doOnce = true;
-    //            Debug.DrawRay(transform.position, Quaternion.AngleAxis(-45, transform.right) * transform.forward * 2, Color.yellow);
-
-    //            r = hit.transform.rotation;
-
-    //            agent.SetDestination(hit.point);
-    //        }
-    //        else if(!doOnce)
-    //        {
-    //            agent.angularSpeed = 180;
-    //            Debug.DrawRay(transform.position, Quaternion.AngleAxis(-45, transform.right) * transform.forward * 2, Color.white);
-    //            if (z > -Mathf.Epsilon)
-    //            {
-    //                velocity = z * agent.speed * transform.forward * Time.deltaTime;
-    //                agent.Move(velocity);
-    //                agent.SetDestination(transform.position + velocity);
-    //            }
-    //            else if (z < -Mathf.Epsilon)
-    //            {
-    //                velocity = z * agent.speed * transform.forward * Time.deltaTime;
-    //                agent.Move(velocity);
-    //                agent.SetDestination(transform.position + velocity);
-    //            }
-    //        }
-
-    //        //When we reach our destination
-    //        if (!agent.pathPending && doOnce)
-    //        {
-    //            if (agent.remainingDistance <= agent.stoppingDistance)
-    //            {
-    //                if (!agent.hasPath && agent.velocity.sqrMagnitude == 0f)
-    //                {
-    //                    Debug.Log("Done");
-    //                    doOnce = false;
-    //                    transform.rotation = hit.transform.rotation;
-    //                }
-    //            }
-    //            Debug.Log(doOnce);
-    //            Debug.Log("pending" + !agent.pathPending);
-    //        }
-    //        else
-    //        {
-    //            transform.Rotate(0, x * agent.angularSpeed * Time.deltaTime, 0);
-    //        }
-    //    }
-    //}
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        // If flipping don't update
+        if (flipping) return;
 
-        int layerMask = 1 << 15;
-
+        Ray ray;
         RaycastHit hit;
+        // Drawing ray to see
+        Debug.DrawRay(transform.position, transform.forward * flipRange, Color.yellow);
 
-        if (Physics.Raycast(transform.position, Quaternion.AngleAxis(0, transform.right) * transform.forward, out hit, 2, layerMask))
+        ray = new Ray(myTransform.position, myTransform.forward);
+        if (Physics.Raycast(ray, out hit, flipRange))
         {
-            Debug.Log("Hit");
-            doOnce = true;
-            Debug.DrawRay(transform.position, Quaternion.AngleAxis(0, transform.right) * transform.forward * 2, Color.yellow);
-            
-            transform.rotation = hit.transform.rotation;
-            transform.position = hit.transform.position;
-            transform.Translate(Vector3.up * 0.5f);
+            // Wall ahead?
+            // Yes: jump to the wall
+            FlipToWall(hit.point, hit.normal);
+        }
+        else if (isGrounded)
+        { 
+            // No: if grounded, don't do anything  
+        }
+
+        // Update surface normal and isGrounded:
+        // Cast ray downwards
+        ray = new Ray(myTransform.position, -myNormal);
+        if (Physics.Raycast(ray, out hit))
+        { 
+            // Use it to update myNormal and isGrounded
+            isGrounded = hit.distance <= distGround + deltaGround;
+            surfaceNormal = hit.normal;
         }
         else
         {
-            Debug.DrawRay(transform.position, Quaternion.AngleAxis(0, transform.right) * transform.forward * 2, Color.white);
-            agent.isStopped = false;
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);
+            isGrounded = false;
+            // Assume usual ground normal to avoid "falling forever"
+            surfaceNormal = Vector3.up;
         }
+
+        // Lerping
+        myNormal = Vector3.Lerp(myNormal, surfaceNormal, lerpSpeed * Time.deltaTime);
+        // Find forward direction with new myNormal:
+        Vector3 myForward = Vector3.Cross(myTransform.right, myNormal);
+        // Align character to the new myNormal while keeping the forward direction:
+        Quaternion targetRot = Quaternion.LookRotation(myForward, myNormal);
+        myTransform.rotation = Quaternion.Lerp(myTransform.rotation, targetRot, lerpSpeed * Time.deltaTime);
+
+        //Movement code - turn left/right with Horizontal axis:
+        myTransform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        //Move the character forth/back with Vertical axis:
+        myTransform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
     }
+
+    private void FlipToWall(Vector3 point, Vector3 normal)
+    {
+        // Signal it's flppnig to wall
+        flipping = true;
+        // Disable physics while jumping
+        rigidbody.isKinematic = true;
+        Vector3 orgPos = myTransform.position;
+        Quaternion orgRot = myTransform.rotation;
+        // Will flip to 0.5 above wall, so were not inside the wall
+        Vector3 dstPos = point + normal * (distGround + 0.5f);
+        Vector3 myForward = Vector3.Cross(myTransform.right, normal);
+        Quaternion dstRot = Quaternion.LookRotation(myForward, normal);
+
+        StartCoroutine(flipTime(orgPos, orgRot, dstPos, dstRot, normal));
+    }
+
+    private IEnumerator flipTime(Vector3 orgPos, Quaternion orgRot, Vector3 dstPos, Quaternion dstRot, Vector3 normal)
+    {
+        for (float t = 0.0f; t < 1.0f;)
+        {
+            t += Time.deltaTime * flipSpeed;
+            myTransform.position = Vector3.Lerp(orgPos, dstPos, t);
+            myTransform.rotation = Quaternion.Slerp(orgRot, dstRot, t);
+            yield return null; 
+        }
+        // Update myNormal
+        myNormal = normal;
+        // Enable physics
+        rigidbody.isKinematic = false;
+        // Jumping to wall finished
+        flipping = false; 
+
+    }
+
 }
